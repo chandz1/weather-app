8dc0ea6c73f1ce3bf1d60d505788031555860312
Sarath <sarathchandragodithi@gmail.com>
2020-07-07 11:19:12 +0300

    Started With The Program... Done with buttons and heading parts of tkinter...

--- LoginSystem.py
+++ LoginSystem.py
@@ -1,17 +1,23 @@
 import tkinter as tk
-import tkinter
 
 
-root = tkinter.Tk()
-screenWidth = root.winfo_screenwidth()
-screenHeight = root.winfo_screenheight()
-canvasProp = {'width': screenWidth, 'height': screenHeight, 'bg': '#e4b119'}
-root.geometry(f'{screenWidth}x{screenHeight}')
-root.maxsize(screenWidth, screenHeight)
-# canvas = tkinter.Canvas(root, cnf=canvasProp).pack()
-frame = tkinter.Frame(root, bg="black", width=500,
-                      height=500).grid(padx=50, pady=125)
-# frame.pack(expand=True)
-
+root = tk.Tk()
+root.title('The Weather Forecast App')
+# screenWidth = root.winfo_screenwidth()
+# screenHeight = root.winfo_screenheight()
+root.geometry('1280x720')
+# root.maxsize(screenWidth, screenHeight)
+frame = tk.Frame(root, bg="#0e95ec", padx=300, pady=150)
+frame.pack(expand=True)
+Header = tk.Label(frame, text='Weather Forecast - Login', bg='#0e95ec')
+Header.config(font=('Ariel', '36'))
+Header.grid(padx=25, pady=25, row=0, column=0)
+btn1 = tk.Button(frame, text='Login', bg="#e3d6d6", width=20)
+btn1.grid(padx=5, pady=5, row=1, column=0)
+btn2 = tk.Button(frame, text='Create Account', bg="#e3d6d6", width=20)
+btn2.grid(padx=5, pady=5, row=2, column=0)
+btn3 = tk.Button(frame, text='Continue Without Sign-In', bg="#e3d6d6",
+                 width=20)
+btn3.grid(padx=5, pady=5, row=3, column=0)
 
 root.mainloop()
