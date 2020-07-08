cb42339fa940e7e8f7d0ce50f489d0170c278a50
Sarath <sarathchandragodithi@gmail.com>
2020-07-08 17:21:02 +0300

    Added Quit  Button and also made quit confirmation dialog...

--- LoginSystem.py
+++ LoginSystem.py
@@ -1,7 +1,9 @@
 import tkinter as tk
+from tkinter import ttk
 from PIL import Image, ImageTk
 
 
+# Classes:
 class addButtons(tk.Canvas):
     def __init__(self, text, width, rely, relx=0.5, height=1,
                  bgcolor='#e3d6d6', command=None):
@@ -16,8 +18,29 @@ class addButtons(tk.Canvas):
 
     def createButton(self):
         button = tk.Button(bgCanvas, text=self.text, width=self.width,
-                           height=self.height, bg=self.bgcolor)
+                           height=self.height, bg=self.bgcolor,
+                           command=self.command)
         button.place(relx=self.relx, rely=self.rely, anchor=tk.CENTER)
+
+
+# Functions:
+def confirmDialog():
+    quitDialog = tk.Toplevel()
+    quitDialog.title('Quit Confirmation')
+    quitDialog.geometry('300x90')
+
+    def cancelQuit():
+        quitDialog.destroy()
+    askLabel = tk.Label(quitDialog, text='Are you sure you want to exit?')
+    askLabel.place(relx=0.5, rely=0.25, anchor=tk.CENTER)
+    nobtn = tk.Button(quitDialog, text='Cancel', width=7, command=cancelQuit)
+    nobtn.place(relx=0.35, rely=0.65, anchor=tk.CENTER)
+    yesbtn = tk.Button(quitDialog, text='Exit', width=7, command=Quit)
+    yesbtn.place(relx=0.65, rely=0.65, anchor=tk.CENTER)
+
+
+def Quit():
+    root.destroy()
 
 
 # Initial Window Creation:
@@ -39,12 +62,14 @@ bgCanvas.create_text(678, 200, anchor=tk.CENTER, font=(
     'Calibre', '28'), text='Weather Forecast - Login')
 
 # Creating Buttons:
-btn1 = addButtons('Sign-In', 20, rely=0.45)
-btn2 = addButtons('Sign-Up', 20, rely=0.5)
-btn3 = addButtons('Continue Without Sign-In', 20, rely=0.55)
+btn1 = addButtons('Sign-In', 20, 0.45)
+btn2 = addButtons('Sign-Up', 20, 0.5)
+btn3 = addButtons('Continue Without Sign-In', 20, 0.55)
+btn4 = addButtons('Quit', 10, 0.977, relx=0.96, command=confirmDialog)
 btn1.createButton()
 btn2.createButton()
 btn3.createButton()
+btn4.createButton()
 
 
 root.mainloop()
