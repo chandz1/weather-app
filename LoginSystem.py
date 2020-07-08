38874475b8bb7c6f6dba472dce24e67bd4d5b2f3
Sarath <sarathchandragodithi@gmail.com>
2020-07-08 11:20:21 +0300

    You know what...

--- LoginSystem.py
+++ LoginSystem.py
@@ -1,25 +1,25 @@
-import tkinter as tk
+from tkinter import *
+from PIL import Image, ImageTk
 
-
-root = tk.Tk()
+root = Tk()
 root.title('The Weather Forecast App')
 screenWidth = root.winfo_screenwidth()
 screenHeight = root.winfo_screenheight()
 root.geometry('{}x{}'.format(screenWidth, screenHeight))
 root.maxsize(screenWidth, screenHeight)
-windowWidth = root.winfo_reqwidth()
-windowHeight = root.winfo_reqheight()
-frame = tk.Frame(root, bg="#0e95ec", padx=200, pady=200)
-frame.grid(padx=windowWidth * 1.5, pady=windowHeight / 2)
-Header = tk.Label(frame, text='Weather Forecast - Login', bg='#0e95ec')
-Header.config(font=('Ariel', '20'))
-Header.grid()
-btn1 = tk.Button(frame, text='Login', bg="#e3d6d6", width=20)
-btn1.grid(padx=5, pady=5, row=1, column=0)
-btn2 = tk.Button(frame, text='Create Account', bg="#e3d6d6", width=20)
-btn2.grid(padx=5, pady=5, row=2, column=0)
-btn3 = tk.Button(frame, text='Continue Without Sign-In', bg="#e3d6d6",
-                 width=20)
-btn3.grid(padx=5, pady=5, row=3, column=0)
+bgImage = ImageTk.PhotoImage(Image.open('CloudsBg.gif'))
+bgCanvas = Canvas(root, bg='#15adc2')
+bgCanvas.pack(expand=True, fill=BOTH, side=TOP)
+bgCanvas.create_image(450, 250, image=bgImage)
+bgCanvas.create_text(150, 150, text='Weather Forecast - Login')
+# Header.config(font=('Ariel', '20'))
+# Header.pack(side=TOP, expand=True)
+btn1 = Button(bgCanvas, text='Login', bg="#e3d6d6", width=20)
+btn1.place(relx=0.5, rely=0.5, anchor=CENTER)
+# btn2 = Button(bgCanvas, text='Create Account', bg="#e3d6d6", width=20)
+# btn2.pack(side=TOP)
+# btn3 = Button(bgCanvas, text='Continue Without Sign-In', bg="#e3d6d6",
+#               width=20)
+# btn3.pack(side=TOP)
 
 root.mainloop()
