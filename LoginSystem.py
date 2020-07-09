630f8df98e0335affa916bc9502d33b59f111e99
sarath2004 <sarathchandragodithi@gmail.com>
2020-07-09 11:43:27 +0300

    minor fixes and login window pop up creation....

--- LoginSystem.py
+++ LoginSystem.py
@@ -3,28 +3,42 @@ from PIL import Image, ImageTk
 
 
 # Classes:
-class addButtons(tk.Canvas):
-    def __init__(self, text, width, rely, relx=0.5, height=1,
-                 bgcolor='#e3d6d6', command=None):
-        super().__init__()
+class addButtons():
+    def __init__(self, master, text, width, rely,
+                 relx=0.5, height=1, command=None):
+        self.master = master
         self.text = text
         self.width = width
         self.height = height
         self.rely = rely
         self.relx = relx
-        self.bgcolor = bgcolor
         self.command = command
 
     def createButton(self):
-        button = tk.Button(bgCanvas, text=self.text, width=self.width,
-                           height=self.height, bg=self.bgcolor,
-                           command=self.command)
+        button = tk.Button(self.master, text=self.text,
+                           width=self.width, height=self.height,
+                           bg='#e3d6d6', command=self.command)
         button.place(relx=self.relx, rely=self.rely, anchor=tk.CENTER)
 
 
 # Functions:
-def loginButtonClicked(root):
-    pass
+def loginButtonClicked():
+    loginWindow = tk.Toplevel(root)
+    loginWindow.grab_set()
+    loginWindow.title('Login')
+    loginWindow.geometry('400x550')
+    usernameInput = tk.Entry(loginWindow)
+    usernameInput.place(relx=0.65, rely=0.45, anchor=tk.CENTER)
+    passwordInput = tk.Entry(loginWindow)
+    passwordInput.place(relx=0.65, rely=0.55, anchor=tk.CENTER)
+    Loginbtn = addButtons(loginWindow, 'Login', 10, 0.65,
+                          command=getLoginInput)
+    btn.createButton()
+
+
+def getLoginInput():
+    text = inputField.get()
+    print(text)
 
 
 def confirmDialog():
@@ -64,13 +78,14 @@ bgCanvas = tk.Canvas(root, bg='#15adc2')
 bgCanvas.pack(expand=True, fill=tk.BOTH, side=tk.TOP)
 bgCanvas.create_image(450, 250, image=bgImage)
 bgCanvas.create_text(675, 200, anchor=tk.CENTER, font=(
-    'Calibre', '28'), text='Weather Forecast - Login')
+    'Source Code Pro', '28'), text='Weather Forecast - Login')
 
 # Creating Buttons:
-btn1 = addButtons('Sign-In', 20, 0.45)
-btn2 = addButtons('Sign-Up', 20, 0.5)
-btn3 = addButtons('Continue Without Sign-In', 20, 0.55)
-btn4 = addButtons('Quit', 10, 0.977, relx=0.96, command=confirmDialog)
+btn1 = addButtons(bgCanvas, 'Login', 20, 0.45, command=loginButtonClicked)
+btn2 = addButtons(bgCanvas, 'Sign Up', 20, 0.5)
+btn3 = addButtons(bgCanvas, 'Continue Without Sign-In', 20, 0.55)
+btn4 = addButtons(bgCanvas, 'Quit', 10, 0.977,
+                  relx=0.96, command=confirmDialog)
 btn1.createButton()
 btn2.createButton()
 btn3.createButton()
