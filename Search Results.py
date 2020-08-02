5e887789db317924a146c51c6f659fd5c4ff2f60
Sarath <sarathchandragodithi@gmail.com>
2020-08-02 15:23:24 +0300

    Search Results

--- Search Results.py
+++ Search Results.py
@@ -3,10 +3,22 @@ import requests
 from bs4 import BeautifulSoup
 
 # Variables:
-
+place = 'hyderabad'
 
 # Functions:
+
+
 def get_url():
-    try:
-        get_access = requests.get(
-            f'https://www.timeanddate.com/weather/?query={place}').text
+    get_access = requests.get(
+        f'https://www.timeanddate.com/weather/?query={place}').text
+    src = BeautifulSoup(get_access, 'html.parser')
+    table_data = src.find(
+        'table', attrs={'class': 'zebra fw tb-theme'})
+    all_places = table_data.find_all('tr')
+    del all_places[0]
+    for i in all_places:
+        names = i.find('td')
+        print(names.text)
+
+
+get_url()
