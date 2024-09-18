18dac031d39a71c7407bb88c1dc5042950be1732
Sarath Chandra Godithi <sarathchandragodithi@gmail.com>
2024-09-19 01:26:09 +0300

    chore: Update .gitignore with venv

--- Getlocation.py
+++ Getlocation.py
@@ -4,10 +4,8 @@ from requests import get
 
 
 def get_location():
-    ip = get('https://api.ipify.org').text
-    send_url = (
-        "http://ipwhois.app/json/{}".format(ip)
-    )
+    ip = get("https://api.ipify.org").text
+    send_url = "http://ipwhois.app/json/{}".format(ip)
     geo_req = requests.get(send_url)
     geo_json = json.loads(geo_req.text)
     city = geo_json["city"]
--- UrlScrapper.py
+++ UrlScrapper.py
@@ -1,17 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-place = ''
+place = ""
 index = 0
 
 
 def scrape_url():
     global url_prefix
-    url = requests.get(
-        f"https://www.timeanddate.com/weather/?query={place}").text
-    source = BeautifulSoup(url, 'html.parser')
+    url = requests.get(f"https://www.timeanddate.com/weather/?query={place}").text
+    source = BeautifulSoup(url, "html.parser")
     table = source.find("table", attrs={"class": "zebra fw tb-theme"})
-    all_places = table.find_all('tr')
+    all_places = table.find_all("tr")
     place_data = all_places[index + 1]
-    link = place_data.find('a')
-    url_prefix = link.get('href')
+    link = place_data.find("a")
+    url_prefix = link.get("href")
